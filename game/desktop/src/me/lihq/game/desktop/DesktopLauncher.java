75cda6305c9383fe2925353bca43b5a9b484ae19
Brooke Hatton <hatton.brooke@gmail.com>
2016-11-17 19:19:16 +0000

    set screen size

--- game/desktop/src/me/lihq/game/desktop/DesktopLauncher.java
+++ game/desktop/src/me/lihq/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import me.lihq.game.GameMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Game";
+		config.width = 1000;
+		config.height = 750;
 		new LwjglApplication(new GameMain(), config);
 	}
 }
