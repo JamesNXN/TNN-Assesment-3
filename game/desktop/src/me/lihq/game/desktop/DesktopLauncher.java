fa2648ca75931cdb43890cf8a41085a7f04eb56d
Brooke Hatton <hatton.brooke@gmail.com>
2016-11-17 19:06:05 +0000

    Start a fresh

--- game/desktop/src/me/lihq/game/desktop/DesktopLauncher.java
+++ game/desktop/src/me/lihq/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package me.lihq.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import me.lihq.game.MainGame;
+import me.lihq.game.GameMain;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MainGame(), config);
+		new LwjglApplication(new GameMain(), config);
 	}
 }
