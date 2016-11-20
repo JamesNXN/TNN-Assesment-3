ed355a3455daac4bc947275a88548eb03bdad11c
Jason Mashinchi <jasonmash@users.noreply.github.com>
2016-11-20 14:19:27 +0000

    Testing (#19)
    Add testing

---
+++ game/tests/src/ExampleUnitTests.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ExampleUnitTests {
+
+    @Test
+    public void exampleTest() {
+        assertEquals(1, 1);
+    }
+
+}
