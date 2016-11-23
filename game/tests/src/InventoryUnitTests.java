c746324e4ea58a397eb190b01b37a006033976ce
Brooke Hatton <hatton.brooke@gmail.com>
2016-11-23 17:57:02 +0000

    fix tests

--- game/tests/src/InventoryUnitTests.java
+++ game/tests/src/InventoryUnitTests.java
@@ -1,27 +1,29 @@
 import me.lihq.game.models.Inventory;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by joeshuff on 23/11/2016.
  */
-public class InventoryUnitTests {
+public class InventoryUnitTests
+{
 
-    Inventory inv = null;
+    Inventory inv;
 
     @Before
     public void before()
     {
         inv = new Inventory();
+        inv.addItem(new Inventory.Item("Test Item", 1, 1));
     }
 
     @Test
     public void testHasItem()
     {
-        inv.addItem(new Inventory.Item("Test Item", 1, 1));
         assertEquals("Fail - Cannot Find Inventory Item by Name", inv.hasItem("Test Item"), true);
-        assertEquals("Fail - Cannot Find Inventory Item by Item", inv.hasItem(new Inventory.Item("Test Item", 1, 1)));
+        assertEquals("Fail - Cannot Find Inventory Item by Item", inv.hasItem(new Inventory.Item("Test Item", 1, 1)), true);
     }
 
 }
