10a305fc837ee90bc45127ebe57038e7abc221cb
Brooke Hatton <hatton.brooke@gmail.com>
2016-11-26 16:35:03 +0000

    Add docs for GameMain

--- game/core/src/me/lihq/game/GameMain.java
+++ game/core/src/me/lihq/game/GameMain.java
@@ -15,22 +15,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class GameMain extends Game {
-    FPSLogger FPS;
-    private GameScreen screen1;
-
+/**
+ * This is the class responsible for the game as a whole. It manages the current states and entry points of the game
+ */
+public class GameMain extends Game
+{
     //This is a static reference to itself. Comes in REALLY handy when in other classes that don't have a reference to the main game
     public static GameMain me = null;
-
     //Game wide variables
     public List<NPC> NPCs = new ArrayList<NPC>();
     public List<Room> rooms = new ArrayList<Room>();
 
-    public int currentRoom = 1;
+    FPSLogger FPS;
+    private GameScreen screen1;
 
-
-	@Override
-	public void create () {
+    @Override
+    /**
+    * This is called at start up. It initialises the game.
+     */
+    public void create()
+    {
 
         initialiseAllData();
 
@@ -39,43 +43,41 @@ public class GameMain extends Game {
         me = this;
 
         this.setScreen(screen1);
-        
+
         FPS = new FPSLogger();
     }
 
-	@Override
-	public void render () {
+    @Override
+    /**
+     * This defines what's rendered on the screen for each frame.
+     */
+    public void render()
+    {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         FPS.log();
 
-        super.render();
+        super.render(); // This calls the render method of the screen that is currently set
 
-	}
-
-    @Override
-	public void dispose () {
-//		map.dispose();
-
-	}
-
-	public void changeMap(int newMapID, int newX, int newY)
-    {
-        //Fade out
-
-        //Change map
-        //Rerender
-
-        //Fade back in
     }
 
-	public void initialiseAllData()
+    @Override
+    public void dispose()
+    {
+
+    }
+
+
+    /**
+     * Generates all the NPC's, Players and Rooms and maps.
+     */
+    public void initialiseAllData()
     {
         //Add ALL NPCs to the list
         //This is how you initialise an NPC
         {
-            NPC npc = new NPC(4,4,1,"charName.png", true)
+            NPC npc = new NPC(4, 4, 1, "charName.png", true)
                     .setCharacterName("Mr Detective 1")
                     .setAccessory(ACCESSORY.WATCH)
                     .setHairColor(HAIR_COLOR.GINGER)
@@ -88,7 +90,7 @@ public class GameMain extends Game {
         }
 
         {
-            NPC npc = new NPC(4,4,2,"charName2.png", true)
+            NPC npc = new NPC(4, 4, 2, "charName2.png", true)
                     .setCharacterName("Mrs Detective 2")
                     .setAccessory(ACCESSORY.HANDBAG)
                     .setHairColor(HAIR_COLOR.BLACK)
