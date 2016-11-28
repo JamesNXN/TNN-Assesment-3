6816b06b288f041c14ef764ad8385630ed4c7d63
Joe Shuff <joe.shuff@btinternet.com>
2016-11-28 14:49:48 +0000

    Added Vector Class

---
+++ game/core/src/me/lihq/game/models/Vector2Int.java
@@ -0,0 +1,27 @@
+package me.lihq.game.models;
+
+/**
+ * Created by joeshuff on 28/11/2016.
+ */
+public class Vector2Int {
+
+    private int x = 0;
+    private int y = 0;
+
+    public Vector2Int(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return this.x;
+    }
+
+    public int getY()
+    {
+        return this.y;
+    }
+
+}
