93969e3b6be3e3e76b33d4a96229aef79acac478
Brooke Hatton <hatton.brooke@gmail.com>
2016-12-17 23:09:22 +0000

    load at start
    Add it so that the menuScreen loads at start

--- game/core/src/me/lihq/game/GameMain.java
+++ game/core/src/me/lihq/game/GameMain.java
@@ -50,7 +50,7 @@ public class GameMain extends Game
     /**
      * The main menu screen that shows up when the game is first started
      */
-    //private MainMenuScreen MenuScreen;
+    private MainMenuScreen menuScreen;
 
     /**
      * A player object for the player of the game
@@ -77,9 +77,10 @@ public class GameMain extends Game
         //set up the screen and display the first room
         screen1 = new NavigationScreen(this);
         screen1.setTiledMapRenderer(player.getRoom().getTiledMap());
-        this.setScreen(screen1);
+
         //Set up the MainMenu
-        //MenuScreen= new MainMenuScreen(this);
+        menuScreen = new MainMenuScreen(this);
+        this.setScreen(menuScreen);
         //Instantiate the FPSLogger to show FPS
         FPS = new FPSLogger();
 
