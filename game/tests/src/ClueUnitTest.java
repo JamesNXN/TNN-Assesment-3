3fa9ceebe0c618679a38c439f508a27896bf5e6c
suzanne <sp1256@york.ac.uk>
2017-02-14 23:40:59 +0000

    Rename the ClueTest

--- game/tests/src/ClueUnitTest.java
+++ game/tests/src/ClueUnitTest.java
@@ -1,60 +1,107 @@
-//package java;
-//
-//import me.lihq.game.Assets;
-//import me.lihq.game.Settings;
-//import me.lihq.game.models.Clue;
-//import org.junit.Before;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-///**
-// * Created by brookehatton on 02/01/2017.
-// */
-//public class ClueUnitTest extends GameTester
-//{
-//    public Clue shoe, book, glasses;
-//    @Before
-//    public void createClues()
-//    {
-//        Assets.assignGameAssets();
-//        //TODO: use correct assets
-//        shoe = new Clue("Shoe", "I am a shoe", Assets.getArrowDirection("NORTH"));
-//        book = new Clue("book", "I am a book", Assets.getArrowDirection("NORTH"));
-//        glasses = new Clue("glasses", "I am a pair of glasses", Assets.getArrowDirection("NORTH"));
-//    }
-//
-//    @Test
-//    public void testName()
-//    {
-//        assertEquals("Name not what was expected", "Shoe", shoe.getName());
-//        assertEquals("Name not what was expected", "book", book.getName());
-//        assertEquals("Name not what was expected", "glasses", glasses.getName());
-//    }
-//
-//    @Test
-//    public void testDescription()
-//    {
-//        assertEquals("Description not what was expected", "I am a shoe", shoe.getDescription());
-//        assertEquals("Description not what was expected", "I am a book", book.getDescription());
-//        assertEquals("Description not what was expected", "I am a pair of glasses", glasses.getDescription());
-//    }
-//
-//    @Test
-//    public void testTileCoordinates()
-//    {
-//        shoe.setTileCoordinates(10, 20);
-//        assertEquals("tile coordinate x not set correctly", 10, shoe.getTileX());
-//        assertEquals("tile coordinate y not set correctly", 20, shoe.getTileY());
-//        assertEquals("position coordinate x not being updated from the tile coordiantes", 10 * Settings.TILE_SIZE, shoe.getX(), 0f);
-//        assertEquals("position coordinate y not being updated from the tile coordiantes", 20 * Settings.TILE_SIZE, shoe.getY(), 0f);
-//    }
-//
-//    @Test
-//    public void testEquality() {
-//        Clue shoe2 = shoe;
-//        assertEquals("Equality test failing", shoe, shoe2);
-//        assertNotEquals("Equality test always true", shoe, book);
-//    }
-//
-//
-//}
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import me.lihq.game.Settings;
+import me.lihq.game.models.Clue;
+import me.lihq.game.models.Vector2Int;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 2017/2/12.Clue Unit Test
+ */
+public class ClueUnitTest {
+
+    private Clue cluetest;
+    private Clue clue2;
+
+    @Before
+    public void setUp() throws Exception {
+        this.cluetest = new Clue();
+        this.clue2 = new Clue("test2");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.cluetest = null;
+        this.clue2 = null;
+    }
+
+
+    /**
+     *test equals only except same clue.
+     */
+    @Test
+    public void equals() throws Exception {
+        assertTrue("Should not be equal",!cluetest.equals(clue2));
+        clue2 = new Clue();
+        assertTrue("Should be equal",cluetest.equals(clue2));
+        String teststring = "String";
+        assertTrue("Should not be equal",!cluetest.equals(teststring));
+    }
+
+    @Test
+    public void getName() throws Exception {
+        assertNotNull(cluetest.getName());
+        System.out.println(cluetest.getName());
+        assertEquals("name doesn't fit","test",cluetest.getName());
+
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+        assertNotNull(cluetest.getDescription());
+        assertEquals("name doesn't fit","test",cluetest.getDescription());
+
+
+    }
+
+    @Test
+    public void getRelatedNpcIdArray() throws Exception {
+        assertNotNull(cluetest.getRelatedNpcIdArray());
+        Array<Integer> test = new Array<>(new Integer[]{1,2,3,4});
+        assertTrue("ID array doesn't fit",cluetest.getRelatedNpcIdArray().equals(test));
+
+    }
+
+    /**
+     * Test if the setTilePosition has set the collisionBox position
+     */
+
+    @Test
+    public void setTilePosition() throws Exception {
+        cluetest.setTilePosition(1,2);
+        Rectangle  testbox= new Rectangle(0,0,0,0);
+        assertNotEquals(testbox,cluetest.getCollisionBox());
+
+    }
+
+    /**
+     * test if the getTilPostition equals to what has been set.
+     */
+    @Test
+    public void getTilePosition() throws Exception{
+        assertNotNull(cluetest.getTilePosition());
+        Vector2Int testtileCoordinates = new Vector2Int(1,2);
+        cluetest.setTilePosition(1,2);
+        assertEquals(testtileCoordinates,cluetest.getTilePosition());
+    }
+
+    @Test
+    public void getCollisionBox() throws Exception {
+        /**
+         * Base case
+         */
+        assertNotNull(cluetest.getCollisionBox());
+
+        Rectangle  testbox= new Rectangle();
+        assertEquals("box0000 not equal",testbox.setSize(Settings.TILE_SIZE),cluetest.getCollisionBox());
+        cluetest.setTilePosition(1,2);
+
+        assertNotEquals("collisionBox size not changed",testbox,cluetest.getCollisionBox());
+
+    }
+
+}
