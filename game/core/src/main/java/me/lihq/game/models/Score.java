110552806ef36937838b03737a12762db546aa8f
td736 <td736@york.ac.uk>
2017-02-18 16:55:11 +0000

    Score documentation

--- game/core/src/main/java/me/lihq/game/models/Score.java
+++ game/core/src/main/java/me/lihq/game/models/Score.java
@@ -6,6 +6,15 @@ import com.badlogic.gdx.Preferences;
 public class Score {
     private static Score instance = new Score();
 
+    /**
+     * Parameters needed for score:
+     *
+     * highScore - The current highscore.
+     * currentScore - An integer that contains the player's current score.
+     * failedAccusationCount - This integer contains the number of times a false accusation has been made.
+     *
+     */
+
     private int highScore;
     private int currentScore;
 
@@ -17,10 +26,13 @@ public class Score {
         failedAccusationCount = 0;
     }
 
-    public static Score getInstance(){
-        return instance;
-    }
 
+
+    /**
+     * The methods addPoints and subPoints increase or decrease currentScore.
+     *
+     * @param point - The number of points to be added/subtracted form score.
+     */
 
     public void addPoints(int point) {
         currentScore += point;
@@ -30,22 +42,47 @@ public class Score {
         currentScore -= point;
     }
 
+    /**
+     * Getters for use in other classes.
+     */
+
     public int getCurrentScore() {
         return currentScore;
     }
 
-    public void reset() {
-        currentScore = 0;
-        failedAccusationCount = 0;
-    }
-
-    public void failedAccusation(){
-        failedAccusationCount++;
+    public static Score getInstance(){
+        return instance;
     }
 
     public int getFailedAccusationCount() {
         return failedAccusationCount;
     }
+
+    /**
+     * This method is called if the player wishes to restart the game to reset the score.
+     */
+
+     public void reset() {
+        currentScore = 0;
+      failedAccusationCount = 0;
+    }
+
+    /**
+     * This method is called when the player falsely accuses an NPC to increase the failed accusation count.
+     */
+
+    public void failedAccusation(){
+        failedAccusationCount++;
+    }
+
+    /**
+     * @param time - The time it took to solve the murder.
+     *
+     * This method applies a time penalty to the current score.
+     * If the current score is greater than the high score, the high score is updated.
+     *             
+     * Returns the integer finalScore.
+     */
 
     public int returnFinalScore(int time ){
         int finalScore = currentScore - time;
